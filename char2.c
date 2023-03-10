7dfb0ba77ed4ed660782a4cf40a3d03d3a30917c
971023als <971023als@naver.com>
2023-03-10 10:30:50 +0900

    Update char2.c

--- char2.c
+++ char2.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("%d %d %d %d", 2 * 2, 2 * 3, 2 * 4, 2 * 5);
-	printf("10 + 20 =30 ");
-	printf(" % d + % d = %d", 10, 20, 30);
+	printf("%d %d %d %d\n", 2 * 2, 2 * 3, 2 * 4, 2 * 5);
+	printf("10 + 20 = 30\n ");
+	printf("%d + %d = %d\n", 10, 20, 30);
 	return 0;
+}
