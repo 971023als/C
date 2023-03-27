1aa34b15bf223434e2649f807e75f5e9703fa323
971023als <971023als@naver.com>
2023-03-27 11:22:44 +0900

    Create for.c

---
+++ for.c
@@ -0,0 +1,21 @@
+//****************
+//	2023-03-27
+//****************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+
+int main(void) {
+	int n;
+	printf("n = ");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		printf("왜 어린이날만 공휴일일까?\n");
+		printf("어버이날, 스승의 날도 놀게 해 주세요.\n");
+	}
+
+	return 0;
+}
