c9e811a8c29c8fd543d0ac62c85024c21330d0bf
971023als <971023als@naver.com>
2023-03-27 11:21:40 +0900

    Create 27for.c

---
+++ 27for.c
@@ -0,0 +1,21 @@
+//****************
+//	2023-03-27
+//****************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+
+int main(void) {
+	for (int i = 1; i <= 10; i++) {
+		printf("반복문");
+		printf("입니다(1).\n");
+	}
+	printf("\n");
+	for (int i = 1; i <= 10; i++)
+		printf("반복문입니다(2).\n");
+
+	return 0;
+}
