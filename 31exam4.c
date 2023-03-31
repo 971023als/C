0d93d6365e54e639921999eb3be20ee092e0d4a5
971023als <971023als@naver.com>
2023-03-31 11:48:03 +0900

    Update 31exam4.c

--- 31exam4.c
+++ 31exam4.c
@@ -4,11 +4,31 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdio.h>
 #include <Windows.h>
 #include <stdbool.h>
 
-int main(void) {
+int main() {
+    int id;
 
-	return 0;
+    while (1) {
+        printf("ID>> ");
+        scanf("%d", &id);
+
+        int first_digit = id / 10000;
+
+        if (first_digit == 1) {
+            printf("학생\n");
+        }
+        else if (first_digit == 2) {
+            printf("교수\n");
+        }
+        else if (first_digit == 3) {
+            printf("직원\n");
+        }
+        else {
+            printf("강사\n");
+        }
+    }
+
+    return 0;
 }
