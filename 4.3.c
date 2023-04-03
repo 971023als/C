ce7294ec6fbbf09eab86cc5ed22c1594d0ade116
971023als <971023als@naver.com>
2023-04-03 11:50:58 +0900

    Create 4.3.c

---
+++ 4.3.c
@@ -0,0 +1,29 @@
+//****************
+//	2023-04-03
+//****************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+
+int main(void) {
+	char gender;
+
+	printf("input a char>>");
+	scanf_s("%c", &gender, 1);
+
+	switch (gender) {
+
+	case 'M': printf("남자입니다."); break;
+	case 'm': printf("남자입니다."); break;
+	case 'f': printf("여자입니다."); break;
+	case 'F': printf("여자입니다."); break;
+
+	default: print("잘못 입력했습니다.\n"); break;
+
+	}
+	
+
+	return 0;
+}
