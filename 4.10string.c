4a924c17342c329f6a6127961a6ff73d1021b40c
971023als <971023als@naver.com>
2023-04-10 10:45:11 +0900

    Create 4.10string.c

---
+++ 4.10string.c
@@ -0,0 +1,16 @@
+//****************
+//	2023-04-10
+//****************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+
+int main(void) { // 1번
+
+	char str[100] = "This is a string.";
+	printf("문자 배열 str에 저장된 문자열은 : %s\n", str);
+
+	return 0;
+}
