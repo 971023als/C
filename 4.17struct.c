2b289fab436ae5ba370748cc1fe5ca335801f1da
971023als <971023als@naver.com>
2023-04-17 11:51:30 +0900

    ok

---
+++ 4.17struct.c
@@ -0,0 +1,18 @@
+//****************
+//	2023-04-17
+//****************
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    int nums[5] = { 1, 2, 3, 4, 5 };
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
