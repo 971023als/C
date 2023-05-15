16f159b8470d5467d989c7b19c95987d2afb48aa
971023als <971023als@naver.com>
2023-05-15 11:42:30 +0900

    Update 5.15star.c

--- 5.15star.c
+++ 5.15star.c
@@ -22,7 +22,7 @@ int main(void){
     for (int i = 1; i <= 5; i++) {
         printf("i = % d:", i);
         for (int j = 1; j <= i; j++) {
-            printf("(% d, % d)", I, j);
+            printf("(% d, % d)", i, j);
         }
         printf("\n");
     }
