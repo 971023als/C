e8a900693b9c4f89cd1488bd8bd67f7bcc5138d8
971023als <971023als@naver.com>
2023-05-15 10:22:06 +0900

    Create 5.15goo.c

---
+++ 5.15goo.c
@@ -0,0 +1,22 @@
+//****************
+//	2023-05-15
+//****************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h> 
+
+
+int main(void) {
+    for (int i = 3; i <= 5; i++) {
+        printf("<구구단 %d단>\n", i);
+        for (int j = 1; j <= 19; j++) {
+            printf("%d * %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
