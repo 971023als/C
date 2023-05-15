58077cc43c369abc7055980731dd2ca17b9aaf0c
971023als <971023als@naver.com>
2023-05-15 11:39:53 +0900

    Create 5.15star.c

---
+++ 5.15star.c
@@ -0,0 +1,30 @@
+//****************
+//	2023-05-15
+//****************
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h> 
+
+
+int main(void){
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
